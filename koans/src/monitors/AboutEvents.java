1d4d1c6445a0ae073a50b8a8814d474163edd1bf
Jacob Tomaw <JACOB.TOMAW@orbitz.com>
2013-11-01 10:19:07 -0500

    Added first koan and modified framework to use other jars.

--- koans/src/monitors/AboutEvents.java
+++ koans/src/monitors/AboutEvents.java
@@ -1,14 +1,23 @@
 package monitors;
 
+import static com.sandwich.koan.constant.KoanConstants.__;
+import static com.sandwich.util.Assert.assertEquals;
 import static com.sandwich.util.Assert.fail;
 
+import com.orbitz.monitoring.api.Attribute;
+import com.orbitz.monitoring.api.Monitor;
+import com.orbitz.monitoring.api.monitor.EventMonitor;
 import com.sandwich.koan.Koan;
 
 public class AboutEvents {
 
     @Koan
     public void eventNaming(){
-        fail("delete this line");
+        Monitor eventMonitor = new EventMonitor("NewEvent");
+        //Monitors are maps of attributes to be processed.
+        //The minimum data that all monitors have is a name. The Attribute class defines many common
+        //keys to the monitors attribute map.
+        assertEquals(eventMonitor.get(Attribute.NAME), __);
     }
     
     @Koan
